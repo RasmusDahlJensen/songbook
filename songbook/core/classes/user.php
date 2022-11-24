4a25f043fc113a744a288f59d17c3b42de8f0893
Rasmus Jensen <rasmusdj96@gmail.com>
2022-11-24 10:30:13 +0100

    added fixme

--- songbook/core/classes/user.php
+++ songbook/core/classes/user.php
@@ -22,6 +22,7 @@ class User{
         return $this->db->query($sql);
     }
 
+        //FIXME: can't get details to show for individual users
     //User details
     public function details($id) {
 		$params = array(
